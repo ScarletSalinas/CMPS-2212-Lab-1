6ec280275ca236daa4a1dbdc97e9035236bed06b
Scarlet Salinas <2022156127@ub.edu.bz>
2026-02-03 15:55:29 -0600

    Completed exercise 6

---
+++ exercise5.js
@@ -0,0 +1,27 @@
+//Scarlet Salinas
+// CMPS2212 Lab 1
+// 03/02/26
+//File: exercise4.js
+
+// Exercise 6
+
+const vehicle = {
+    wheels: 4,
+    describe() {
+    return `This vehicle has ${this.wheels} wheels`;
+    }
+};
+
+// Create a car object using Object.create(vehicle)
+const car = Object.create(vehicle);
+
+// Properties of car
+car.make = "Toyota";
+car.model = "Camry",
+car.year = 2024;
+
+// Call the describe() method on car (inherited from vehicle)
+console.log(car.describe()); // "This vehicle has 4 wheels"
+console.log(Object.hasOwn(car, 'make')); // true
+console.log(Object.hasOwn(car, 'wheels')); // false
+console.log(car.wheels); // 4 (from prototype)
