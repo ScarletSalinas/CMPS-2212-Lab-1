190d94df9e5dc50a7a76362556b85fb58e4b3daa
Scarlet Salinas <2022156127@ub.edu.bz>
2026-02-03 15:43:02 -0600

    Implemented Exercise 4

---
+++ exercise3.js
@@ -0,0 +1,40 @@
+//Scarlet Salinas
+// CMPS2212 Lab 1
+// 03/02/26
+//File: exercise2.js
+
+// Exercise 4
+const student = {
+    firstName: "John",
+    lastName: "Doe",
+    studentId: "12345",
+    courses: [],
+
+    // Methods:
+
+    // getFullName() returns full name
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+
+    // enrollCourse(courseName) adds a course to  courses array
+    enrollCourse(courseName) {
+        this.courses.push(courseName);
+    },
+
+    // getCourseCount() returns the number of enrolled courses
+    getCourseCount() {
+        return this.courses.length;
+    }
+};
+
+
+
+// Testing 
+console.log(student.getFullName()); // Expected: John Doe
+student.enrollCourse('CMPS2212');
+student.enrollCourse('MATH2210');
+console.log(student.getCourseCount()); // Expected: 2
+//console.log(student.courses);
+
+
